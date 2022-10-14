288d57dbca3c56f6ec2797018f49adb8034bd7c8
김형범 201611067 <fud1020@likelion.org>
2022-10-15 00:32:16 +0900

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def main():
     print('#training images = %d' % dataset_size)
 
     model = create_model(opt)
-    visualizer = Visualizer(opt)
+    #visualizer = Visualizer(opt)
     total_steps = 0
 
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
@@ -25,7 +25,7 @@ def main():
 
         for i, data in enumerate(dataset):
             iter_start_time = time.time()
-            visualizer.reset()
+            #visualizer.reset()
             total_steps += opt.batchSize
             epoch_iter += opt.batchSize
             model.set_input(data)
@@ -33,14 +33,14 @@ def main():
 
             if total_steps % opt.display_freq == 0:
                 save_result = total_steps % opt.update_html_freq == 0
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_steps % opt.print_freq == 0:
                 errors = model.get_current_errors()
                 t = (time.time() - iter_start_time) / opt.batchSize
-                visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-                if opt.display_id > 0:
-                    visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+                #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+                #if opt.display_id > 0:
+                    #visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
             if total_steps % opt.save_latest_freq == 0:
                 print('saving the latest model (epoch %d, total_steps %d)' %
